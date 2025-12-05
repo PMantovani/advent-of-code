82519873131b0dbd5422eb43ee1c3fb5efb34c5f
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2025-12-05 16:19:11 -0300

    day 3 - part 2

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -2,6 +2,7 @@ use std::{fs};
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -31,7 +32,44 @@ fn part_1() {
         num_in_str.push(max_second);
         banks_joltage.push(u32::from_str_radix(num_in_str.as_str(), 10).unwrap_or(0));
     }
+    // println!("{:?}", banks_joltage);
+    // let total: u32 = banks_joltage.iter().sum();
+    // println!("{:?}", total);
+}
+
+
+fn part_2() {
+    let contents = fs::read_to_string("./assets/input.txt")
+    .expect("Should have been able to read the file");
+
+    let lines = contents.lines();
+    let mut banks_joltage: Vec<u64> = Vec::new();
+
+    
+    for line in lines {
+        let mut built_number = String::new();
+        let chars = line.chars();
+        let mut curr_digit = 0;
+        let mut pending_chars = chars.clone();
+
+        while curr_digit < 12 {
+            let available_length = pending_chars.clone().count();
+            let max = pending_chars.clone().enumerate().fold(
+                ('0', 0 as usize),
+                |acc, (idx, val)|  {
+                    match val > acc.0 && idx < available_length - (12 - curr_digit - 1) {
+                        true => (val, idx),
+                        false => acc,
+                    }
+                } );
+            built_number.push(max.0);
+            curr_digit += 1;
+            pending_chars = pending_chars.as_str()[(max.1+1)..].chars();
+        }
+
+        banks_joltage.push(u64::from_str_radix(built_number.as_str(), 10).unwrap_or(0));
+    }
     println!("{:?}", banks_joltage);
-    let total: u32 = banks_joltage.iter().sum();
+    let total: u64 = banks_joltage.iter().sum();
     println!("{:?}", total);
 }
