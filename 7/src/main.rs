5e7464a1ad33cafc0f0ea0413e0db2f2151300f4
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2025-12-08 16:37:14 -0300

    day 7 - part 2

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -1,7 +1,8 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
-    part_1();
+    // part_1();
+    part_2();
 }
 
 #[allow(dead_code)]
@@ -40,4 +41,42 @@ fn part_1() {
 
     println!("{:?}", split_count);
 
+}
+
+fn part_2() {
+
+    let contents = fs::read_to_string("./assets/input.txt")
+    .expect("Should have been able to read the file");
+
+    let lines: Vec<_> = contents.lines().collect(); // collect lines to avoid moving iterator
+    let lines_bytes: Vec<Vec<u8>> = lines.iter().map(|x| x.as_bytes().to_vec()).collect();
+    let mut memo_map: HashMap<(usize, usize), u64> = HashMap::new();
+
+    let Some(start_col) = lines_bytes[0].iter().position(|x| *x == b'S') else { panic!("S Not found!!!")};
+    println!("{:?}", check_split(&lines_bytes, 1, start_col, &mut memo_map));
+
+}
+
+fn check_split(matrix: &Vec<Vec<u8>>, i: usize, j: usize, memo_map: &mut HashMap<(usize, usize), u64>) -> u64 {
+    let mut curr_row = i;
+
+    while curr_row < matrix.len() {
+        if matrix[curr_row][j] == b'^' {
+            let memo = memo_map.get(&(curr_row, j));
+
+            match memo {
+                Some(val) => return *val,
+                None => {
+                    let result = check_split(&matrix, curr_row + 1, j - 1, memo_map) + check_split(&matrix, curr_row + 1, j + 1, memo_map);
+                    memo_map.insert((curr_row, j), result);
+                    return result;
+                }
+            }
+
+        }
+
+        curr_row += 1;
+    }
+
+    return 1;
 }
