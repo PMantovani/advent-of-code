2c21463d22da50185d886d77d7c41b468f9f5ab8
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2025-12-08 11:01:56 -0300

    day 5 - part 2

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashSet, fs, ops::RangeInclusive};
 
 fn main() {
-    part_1();
-    // part_2();
+    // part_1();
+    part_2();
 }
 
 #[allow(dead_code)]
@@ -32,6 +32,62 @@ fn part_1() {
     println!("{:?}", count);
 
 }
+
+fn part_2() {
+    let contents = fs::read_to_string("./assets/input.txt")
+    .expect("Should have been able to read the file");
+
+    let lines = contents.lines();
+
+    let mut fresh_ranges_str = Vec::new();
+
+    lines.for_each(|line| {
+        if line.contains("-") {
+            fresh_ranges_str.push(line);
+        }
+    });
+
+    let ranges = convert_from_ranges_str(fresh_ranges_str);
+    let mut jointed_sets: Vec<Option<RangeInclusive<u64>>> = ranges.iter().cloned().map(Some).collect();
+
+    ranges.into_iter().enumerate().for_each(|(idx, r)| {
+        let mut cmp_set = r;
+
+        for j in 0..idx {
+            let Some(j_set) = jointed_sets[j].clone() else { continue };
+
+            if cmp_set.start() >= j_set.start() && cmp_set.start() <= j_set.end() {
+                // começo de r está contido em j. Set deve ser começo de j e final do maior entre os dois.
+                let new_end = *cmp_set.end().max(j_set.end());
+                let new_start = *j_set.start();
+                cmp_set = new_start..=new_end;
+                jointed_sets[j] = None;
+            } else if cmp_set.end() <= j_set.end() && cmp_set.end() >= j_set.start() {
+                // final de r está contido em j. Set deve ser final de j, e começo do menor entre os dois.
+                let new_end = *j_set.end();
+                let new_start = *cmp_set.start().min(j_set.start());
+                cmp_set = new_start..=new_end;
+                jointed_sets[j] = None;
+
+            } else if cmp_set.start() <= j_set.start() && cmp_set.end() >= j_set.end() {
+                // j está contido em cmp_set. Set deve ser cmp_set.
+                let new_end = *cmp_set.end();
+                let new_start = *cmp_set.start();
+                cmp_set = new_start..=new_end;
+                jointed_sets[j] = None;
+            }
+            // caso contrário, disjointed set. não mergear.
+        }
+        jointed_sets[idx] = Some(cmp_set);
+    });
+
+    let count: u64 = jointed_sets.iter()
+        .map(|s| s.as_ref().map_or(0, |r| r.end() - r.start() + 1))
+        .sum();
+
+    println!("{:?}", count);
+}
+
 
 fn convert_from_ranges_str(ranges_str: Vec<&str>) -> Vec<RangeInclusive<u64>> {
     let mut ranges = Vec::new();
