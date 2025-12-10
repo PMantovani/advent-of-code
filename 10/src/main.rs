8bdecafca9c90a070af42189d4c319cb6d99e9de
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2025-12-10 15:42:51 -0300

    day 10 - part 2

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -1,15 +1,20 @@
 use regex::Regex;
 use std::fs;
+use std::ops::Add;
+use z3::Solver;
+use z3::ast::Int;
 
 fn main() {
-    part_1();
-    // part_2();
+    // part_1();
+    part_2();
 }
 
 #[derive(Debug)]
 struct Machine {
     desired_lights: u16,
     buttons: Vec<u16>,
+    buttons_indices: Vec<Vec<usize>>,
+    joltages: Vec<u16>,
 }
 
 #[allow(dead_code)]
@@ -25,6 +30,18 @@ fn part_1() {
     println!("{:?}", results);
 }
 
+fn part_2() {
+    let contents =
+        fs::read_to_string("./assets/input.txt").expect("Should have been able to read the file");
+    let machines = process_input(&contents);
+
+    let results = machines
+        .iter()
+        .map(|machine| find_button_presses_for_joltage(machine))
+        .sum::<u64>();
+    println!("{:?}", results);
+}
+
 fn find_button_presses(machine: &Machine) -> Option<usize> {
     let combinations = find_combinations(&machine.buttons);
 
@@ -35,6 +52,43 @@ fn find_button_presses(machine: &Machine) -> Option<usize> {
         }
     }
     None
+}
+
+fn find_button_presses_for_joltage(machine: &Machine) -> u64 {
+    let solver = Solver::new();
+
+    let button_pressed_n = machine
+        .buttons
+        .iter()
+        .enumerate()
+        .map(|(idx, _)| Int::new_const(format!("button_{idx}").as_str()))
+        .collect::<Vec<_>>();
+
+    for (idx, joltage) in machine.joltages.iter().enumerate() {
+        let relevant_button_vars = machine
+            .buttons_indices
+            .iter()
+            .enumerate()
+            .filter(|(_, vals)| vals.contains(&idx))
+            .map(|(btn_idx, _)| &button_pressed_n[btn_idx])
+            .collect::<Vec<_>>();
+
+        let eq_result = joltage;
+        let sum = relevant_button_vars
+            .iter()
+            .fold(Int::from_u64(0), |acc, x| acc.add(*x));
+        solver.assert(sum.eq(*eq_result));
+    }
+
+    button_pressed_n.iter().for_each(|x| solver.assert(x.ge(0)));
+
+    let result: u64 = solver
+        .solutions(button_pressed_n, true)
+        .map(|solution| solution.iter().map(|x| x.as_u64().unwrap()).sum::<u64>())
+        .min()
+        .unwrap();
+    println!("Solution: {:?}", result);
+    result
 }
 
 fn find_combinations(arr: &Vec<u16>) -> Vec<Vec<u16>> {
@@ -92,9 +146,16 @@ fn process_input(contents: &str) -> Vec<Machine> {
                 })
                 .collect::<Vec<_>>();
 
+            let joltages: Vec<_> = captures[3]
+                .split(",")
+                .map(|x| x.parse::<u16>().unwrap())
+                .collect();
+
             Machine {
                 buttons: buttons.iter().map(|x| find_u16_from_usize_vec(x)).collect(),
+                buttons_indices: buttons,
                 desired_lights: find_u16_from_bool_vec(desired_lights),
+                joltages,
             }
         })
         .collect()
